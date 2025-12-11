aac62f1cdb1b2c3156c15355ec8fb70d4e5def4b
KyleDelRegno <kyle@Kyles-MacBook-Pro-6.local>
2025-12-10 21:36:07 -0500

    Cursor System

--- Scripts/controller_clicker.gd
+++ Scripts/controller_clicker.gd
@@ -3,9 +3,14 @@ extends Node2D
 @export var tilemap_script : Node2D
 @export var tilemap_master : TileMapLayer
 
+func CursorLocation():
+	var block_pos = tilemap_script.BlockPosition()
+	$Sprite2D.position = Vector2(block_pos.x, block_pos.y)
+
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and not event.is_pressed():
-			var mouse_pos = get_global_mouse_position()
-			mouse_pos = to_local(mouse_pos)
-			tilemap_script.BlockPlacement()
+			#Place the block 
+			var block_pos = tilemap_script.BlockPosition()
+			if block_pos[0]!=null:
+				tilemap_script.BlockAdd(block_pos[0])
